50c53ceb622a0ebbc055b3396f2e46eb3c6407ea
Jasmijn-Ginger <jasmijn.h@live.nl>
2024-03-20 14:00:47 +0100

    Nieuwe testclass

--- src/test/java/nl/ginger/librarywebsite/presentation/controller/bookControllerTest1.java
+++ src/test/java/nl/ginger/librarywebsite/presentation/controller/bookControllerTest1.java
@@ -7,20 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class HttpRequestTest {
+class bookControllerTest1 {
 
     @LocalServerPort
     private int port;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private TestRestTemplate restTemplate;
 
     @Test
     void greetingShouldReturnDefaultMessage() throws Exception {
